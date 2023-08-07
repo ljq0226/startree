353c40c5b9f992459d5aee6457d793e1b4c9808f
ljq0226 <1035821043@qq.com>
2023-08-07 08:35:40 +0800

    add:post module

---
+++ server/src/module/post/entities/post.entity.ts
@@ -0,0 +1,26 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { User } from 'src/module/user/entities/user.entity';
+
+@ObjectType()
+export class Post {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  content: string;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+
+  @Field()
+  linkUsers: string;
+
+  @Field(() => User, { nullable: true })
+  User: User;
+
+  @Field({ nullable: true })
+  userName: string;
+}
