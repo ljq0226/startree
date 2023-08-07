99df82ea8379407f8f87547d8c5e712ae2b9708d
ljq0226 <1035821043@qq.com>
2023-08-07 08:35:04 +0800

    chore:add graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from 'node:path'
+import { Module } from '@nestjs/common'
+import { PrismaModule } from 'nestjs-prisma'
+import { GraphQLModule } from '@nestjs/graphql'
+import type { ApolloDriverConfig } from '@nestjs/apollo'
+import { ApolloDriver } from '@nestjs/apollo'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { AppService } from './app.service'
+import { AppController } from './app.controller'
+import { UserModule } from './module/user/user.module';
+import { PostModule } from './module/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {},
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
