2a6a18df6cc26d7875c18ef7d888c1e21db2bcc6
ljq0226 <1035821043@qq.com>
2023-08-13 08:09:26 +0800

    config(apollo):delete __typename

--- client/apollo-client.ts
+++ client/apollo-client.ts
@@ -2,5 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 })
