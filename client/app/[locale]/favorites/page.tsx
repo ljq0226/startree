49ee063116b18c772a0c795943a8ef8250ef127b
ljq0226 <1035821043@qq.com>
2023-08-21 22:10:41 +0800

    page(star\like):add skeleton

--- client/app/[locale]/favorites/page.tsx
+++ client/app/[locale]/favorites/page.tsx
@@ -6,32 +6,53 @@ import Aside from '@/components/aside/Aside'
 import { UserStore } from '@/store'
 import type { PostType } from '@/types'
 import Post from '@/components/post/Post'
+import useI18n from '@/hooks/theme/useI18n'
+import Skeleton from '@/components/ui/Skeleton'
 
 export default function App() {
   const { name } = UserStore(s => s.user)
+  const t = useI18n('common')
   const { data, loading } = useQuery(GetLikePost, { variables: { userName: name } })
   const [posts, setPosts] = useState<PostType[]>([])
-
+  const [showData, setShowData] = useState(false)
   useEffect(() => {
     if (!loading)
       setPosts(data.getLikePost)
   }, [data])
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setShowData(true)
+    }, 1000)
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [])
   return (
-    <>
+    <div className='flex w-full overflow-x-hidden overflow-y-auto'>
       <div className="flex flex-col main-container">
         <div className="hidden h-6 xl:block"></div>
         {
-          posts.map((post) => {
-            return (
-              <Post key={post.id} {...post} />
-            )
-          })
+          showData
+            ? <>
+              {
+                posts.map((post) => {
+                  return (
+                    <Post key={post.id} {...post} />
+                  )
+                })
+              }
+              <div className='min-h-[50px] text-center '>{t('end_of_list')}</div>
+            </>
+            : <>
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </>
         }
-        <div className="flex p-6 flex-center">
-          End of the list
-        </div>
+
       </div>
       <Aside />
-    </>
+    </div >
   )
 }
