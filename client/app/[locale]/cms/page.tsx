5d0289813be8d4d1fbc84a2ff263dc52e0165c70
ljq0226 <1035821043@qq.com>
2023-10-06 20:42:39 +0800

    chore:update

--- client/app/[locale]/cms/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>cms page</div>
-  )
-}
-
-export default page
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,8 +7,6 @@ interface Props {
   children: ReactNode
 }
 
-// Since we have a `not-found.tsx` page on the root, a layout file
-// is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
   return children
 }
